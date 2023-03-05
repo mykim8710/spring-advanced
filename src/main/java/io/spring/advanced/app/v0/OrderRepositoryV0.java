eedb6aba5381642fb1154481ffbba6b230278705
mykim <mykim8710@gmail.com>
2023-03-05 20:50:58 +0900

    [ dev ] Spring advanced Study - 예제 Controller -> Service -> Repository 추가

---
+++ src/main/java/io/spring/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,27 @@
+package io.spring.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+    public void save(String itemId) {
+        if(itemId.equals("ex")) {
+            throw new IllegalArgumentException("exception!!");
+        }
+
+        sleep(1000);    //리포지토리는 상품을 저장하는데 약 1초 정도 걸리는 것으로 가정하기 위해 1초 지연을 주었다.(1000ms)
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
